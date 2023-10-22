145f1ead298a98065c9bff924e874c32bc8c6aa4
Yann L <56452324+Brawdunoir@users.noreply.github.com>
2023-10-23 00:49:33 +0200

    Split battery in a separate class (#1)
    * add battery class
    
    * fix compilation battery

---
+++ Battery.cpp
@@ -0,0 +1,28 @@
+#include "Battery.h"
+#include <Arduino.h>
+
+Battery::Battery(int pin, float minV, float maxV)
+    : pin(pin), level(100), minVoltage(minV), maxVoltage(maxV)
+{
+  pinMode(pin, INPUT);
+}
+
+int Battery::getLevel()
+{
+  return level;
+}
+
+void Battery::update()
+{
+  float voltage = readVoltage();
+  level = map(voltage, minVoltage, maxVoltage, 0, 100);
+  level = constrain(level, 0, 100);
+}
+
+float Battery::readVoltage()
+{
+  float voltage = analogRead(pin);
+  voltage *= 5.0;
+  voltage /= 1023.0;
+  return voltage;
+}
